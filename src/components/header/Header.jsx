d402698295c507227370495b0e9e2e9e864d37cc
daleal-pr <daleal.prgmr@gmail.com>
2022-12-09 10:40:49 -0300

    arreglando home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,30 +1,45 @@
 import React from 'react'
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 //Imagenes
 import Perros from '../../assets/img/header/perros.png'
+import PerroCachorro from '../../assets/img/header/perro_cachorro.png'
 
 
 function Header() {
   return (
     <header>
 
-        <div className='header_cont'>
+      <div className='header_cont'>
 
-            <div className='header_textCont'>
-                <h1 className='header_textTitle'>Adopta un perro rescatado</h1>
+        <div className='header_textCont'>
+          <h1 className='header_textTitle'>Adopta un perro rescatado</h1>
 
-                <p className='header_textP'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-                    Rerum, quaerat ab necessitatibus fugit amet ea eos eveniet
-                    autem natus optio sequi quia aspernatur eaque, illo perspiciatis
-                    at, consequuntur quo repudiandae?
-                </p>
+          <p className='header_textP'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Rerum, quaerat ab necessitatibus fugit amet ea eos eveniet
+            autem natus optio sequi quia aspernatur eaque, illo perspiciatis
+            at, consequuntur quo repudiandae?
+          </p>
 
-                <button className='btn_adoptar'><Link className='a_adoptaAqui' to="/perros">Adopta Aqui</Link></button>
-            </div>
+          <button className='btn_adoptar'><Link className='a_adoptaAqui' to="/perros">Adopta Aqui</Link></button>
+        </div>
 
-            <img className='header_img' src={Perros}/>
+        <img className='header_img' src={Perros} />
+
+      </div>
+
+      <div className='header_cot2'>
+        <div className='header_cont2'>
+
+          <img className='header_img2' src={PerroCachorro} />
+
+          <p className='header_textP2'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem perferendis
+            neque corporis eligendi numquam sunt enim fuga recusandae? Suscipit perspiciatis
+            quibusdam incidunt, rerum dolorem non eveniet consequuntur numquam. Quas, sit.
+          </p>
 
         </div>
+
+      </div>
     </header>
   )
 }
